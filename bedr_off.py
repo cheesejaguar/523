19de38f32621225a2d79f4743e16b5c3294180ec
Cheesejaguar <Cheesejaguar@gmail.com>
2013-07-11 11:36:34 -0700

    Added some simple scripts that can be called by cron for different rooms

---
+++ bedr_off.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+execfile("523.py")
+
+for bulb in ['lamp', 'bed', 'book']:
+    bulb.on = False
+
+
+
---
+++ main_on.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+execfile("523.py")
+
+for bulb in ['table', 'entry']:
+    bulb.on = True
+    bulb.xy = [0.35, 0.35]
+
+
+
