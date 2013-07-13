cc0e84329fdd5ade708ffa173db13aa08366a7a3
cheesejaguar <cheesejaguar@gmail.com>
2013-07-13 01:35:27 -0700

    fixed luke's dumb green mistake

--- twister/twister.py
+++ twister/twister.py
@@ -5,15 +5,16 @@ import random, time, pyttsx
 tts = pyttsx.init()
 tts.say('Starting Twister')
 tts.runAndWait()
+colors = [[red, green, blue, yellow], ['red', 'green', 'blue', 'yellow']]
 def namestr(obj, namespace):
 	return [name for name in namespace if namespace[name] is obj]
 pick = lambda items: random.choice(items)
 while not time.sleep(6):
-	color=pick((red, green, blue, yellow))
+	color=pick((0, 1, 2, 3))
 	side=pick(("left", "right"))
 	appendage=pick(("foot", "hand"))
-	main(True,color) 
-	line = "Place " + side + " " + appendage + " on " + namestr(color,globals())[0]
+	main(True,colors[0][color]) 
+	line = "Place " + side + " " + appendage + " on " +colors[1][color]
 	print line
 	tts.say(line)
 	tts.runAndWait()
