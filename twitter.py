7798c647211bfe027b51f33b791e85c5e55c125f
cheesejaguar <cheesejaguar@gmail.com>
2013-07-15 11:36:01 -0700

    Fixed input listing

--- twitter.py
+++ twitter.py
@@ -11,7 +11,10 @@ my_access_token_secret = 'None'
 
 commandHandle = 'None'
 
-api = twitter.Api(consumer_key=my_consumer_key, consumer_secret=my_consumer_secret, access_token_key=my_access_token, access_token_secret=my_access_token_secret)
+api = twitter.Api(consumer_key=my_consumer_key,
+                  consumer_secret=my_consumer_secret,
+                  access_token_key=my_access_token,
+                  access_token_secret=my_access_token_secret)
 
 ## This example just takes the most recent post from a specified twitter user, and sets the color of your room to that
 statuses = api.GetUserTimeline(commandHandle)
